914b5c4830c5ceec98bae2a4e03a3e9f36f8c60d
Damien Robinson <damien.robinson@xcommedia.com.au>
2020-12-12 23:16:50 +1000

    ci(lighthouse): add assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,17 @@ module.exports = {
       url: ['/'],
       staticDistDir: './dist',
     },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'content-width': 'warn',
+        'non-composited-animations': 'warn',
+        'unused-css-rules': 'warn',
+        'unused-javascript': 'warn',
+        'valid-source-maps': 'warn',
+        'uses-rel-preconnect': 'warn',
+      },
+    },
     upload: {
       target: 'temporary-public-storage',
     },
